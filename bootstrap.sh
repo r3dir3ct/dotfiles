2b4ab7a5e6cb13b7a966b7c1b3eb015b4a790c2c
Jiyuan Zheng <jiyuanz95@gmail.com>
2018-12-05 17:20:01 +0800

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+#! /usr/bin/env bash
+DOTFILES_DIR=$HOME/.local/dotfiles/
+
+if [ ! -d $DOTFILES_DIR ]; then
+    mkdir -p $HOME/.local
+    cd $HOME/.local
+    git clone git@github.com:r3dir3ct/dotfiles.git $DOTFILES_DIR
+fi
+
+# rsync files
+function doIt(){
+    rsync --exclude ".git/" \
+          --exclude ".DS_Store" \
+          --exclude ".osx" \
+          --exclude "bootstrap.sh" \
+          --exclude "zsh_init.sh" \
+          --exclude "README.md" \
+          --exclude "LICENSE" \
+          -avh --no-perms $DOTFILES_DIR $HOME;
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
+
+# install tpm and plugins
+if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
+# install oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh $HOME
+    TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+    if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+        # If this platform provides a "chsh" command (not Cygwin), do it, man!
+        if hash chsh >/dev/null 2>&1; then
+            printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+            chsh -s $(grep /zsh$ /etc/shells | tail -1)
+        # Else, suggest the user do so manually.
+        else
+            printf "I can't change your shell automatically because this system does not have chsh.\n"
+            printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+        fi
+    fi
+fi
+
+# init zsh
+if [ ! -f $HOME/.zshrc ]; then
+    echo "source ${DOTFILES_DIR}zsh_init.sh" > $HOME/.zshrc
+fi
+
+
