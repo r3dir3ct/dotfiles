cbadcd63b899b70ffb768af79cf8a2f04f7cc39e
Jiyuan Zheng <jiyuanz95@gmail.com>
2019-01-18 14:50:27 +0800

    add dryrun to rync

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,10 +21,24 @@ function doIt(){
           --exclude "LICENSE" \
           -avh --no-perms $DOTFILES_DIR $HOME;
 }
+# rsync dryrun
+function dryRun(){
+    rsync --dry-run \
+          --exclude ".git/" \
+          --exclude ".DS_Store" \
+          --exclude ".osx" \
+          --exclude "bootstrap.sh" \
+          --exclude "zsh_init.sh" \
+          --exclude "README.md" \
+          --exclude "LICENSE" \
+          -avh --no-perms $DOTFILES_DIR $HOME;
+}
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "These files will be overwrite, please check!"
+    dryRun;
+    read -p "Are you sure to overwrite? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
