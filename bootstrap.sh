8bbb571279ed3d977b59b76196b815bbcbe5e8e4
Jiyuan Zheng <jiyuanz95@gmail.com>
2020-12-15 11:14:07 +0800

    add oh-my-zsh and tpm as git submodules, and mv zsh_init.sh to .zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -60,30 +60,16 @@ else
     echo "Unknown OS, please check!"
 fi
 
-# install tpm and plugins
-if [ ! -d $HOME/.tmux/plugins/tpm ]; then
-    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-fi
-# install oh-my-zsh
-if [ ! -d $HOME/.oh-my-zsh ]; then
-    git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
-    TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-    if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-        # If this platform provides a "chsh" command (not Cygwin), do it, man!
-        if hash chsh >/dev/null 2>&1; then
-            printf "Time to change your default shell to zsh!\n"
-            chsh -s $(grep /zsh$ /etc/shells | tail -1)
-        # Else, suggest the user do so manually.
-        else
-            printf "I can't change your shell automatically because this system does not have chsh.\n"
-            printf "Please manually change your default shell to zsh!\n"
-        fi
+# try to change shell to zsh
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    # If this platform provides a "chsh" command (not Cygwin), do it, man!
+    if hash chsh >/dev/null 2>&1; then
+        printf "Time to change your default shell to zsh!\n"
+        chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # Else, suggest the user do so manually.
+    else
+        printf "I can't change your shell automatically because this system does not have chsh.\n"
+        printf "Please manually change your default shell to zsh!\n"
     fi
 fi
-
-# init zsh
-if [ ! -f $HOME/.zshrc ]; then
-    echo "source ${DOTFILES_DIR}/zsh_init.sh" > $HOME/.zshrc
-fi
-
-
